a4be4a89dd58ddff97466461290f83f831c2d676
Your Name <you@example.com>
2024-04-01 13:37:25 +0000

    Removed extension import

--- groundingdino/models/GroundingDINO/ms_deform_attn.py
+++ groundingdino/models/GroundingDINO/ms_deform_attn.py
@@ -326,29 +326,7 @@ class MultiScaleDeformableAttention(nn.Module):
                     reference_points.shape[-1]
                 )
             )
-    
-        """
-        if torch.cuda.is_available() and value.is_cuda:
-            halffloat = False
-            if value.dtype == torch.float16:
-                halffloat = True
-                value = value.float()
-                sampling_locations = sampling_locations.float()
-                attention_weights = attention_weights.float()
 
-            output = MultiScaleDeformableAttnFunction.apply(
-                value,
-                spatial_shapes,
-                level_start_index,
-                sampling_locations,
-                attention_weights,
-                self.im2col_step,
-            )
-
-            if halffloat:
-                output = output.half()
-        else:
-        """
         output = multi_scale_deformable_attn_pytorch(
             value, spatial_shapes, sampling_locations, attention_weights
         )
